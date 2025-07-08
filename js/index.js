56c49e46e857dfb527dfcbbcd772765c296e92ad
Viktoria <viktoriashtarbanova@gmail.com>
2025-07-08 13:04:46 +0300

    all

--- js/index.js
+++ js/index.js
@@ -5,7 +5,19 @@ document.addEventListener("DOMContentLoaded", function () {
   links.forEach(link => {
     link.addEventListener('click', () => {
       // Премахваме "active" от всички
-      links.forEach(l => l.classList.remove('active'));
+      links.forEach(l => {
+        //close navbar slowly
+        const navbar = document.querySelector('.navbar-collapse');
+        if (navbar && navbar.classList.contains('show')) {
+          navbar.style.transition = 'opacity 0.5s ease';
+          navbar.style.opacity = 0;
+          setTimeout(() => {
+            navbar.classList.remove('show');
+            navbar.style.opacity = 1; // Reset opacity for next open
+          }, 200);
+        }
+        l.classList.remove('active');
+      });
       // Добавяме "active" само на текущия линк
       link.classList.add('active');
     });
